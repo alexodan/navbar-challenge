d34919314cb1050556f1151dfab6e2cdec0a4f3c
Alexis Diaz <alexdan1993@gmail.com>
2023-05-11 12:22:36 -0300

    Adds buttons to add/remove mock item (to test animation issue easily)

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,63 @@ import {
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import "./app.scss";
+import { useState } from "react";
+
+const initialItems = [
+  {
+    icon: faClock,
+    title: "Time",
+  },
+  {
+    icon: faFutbol,
+    title: "Comments",
+  },
+  {
+    icon: faCompass,
+    title: "Compass",
+  },
+  {
+    icon: faUser,
+    title: "User",
+  },
+];
 
 function App() {
+  const [items, setItems] = useState(initialItems);
+
   const handleItemClicked = ({ title, id }: { title: string; id?: number }) => {
     console.log(`Item clicked title ${title} id ${id}`);
   };
 
   return (
     <>
+      <button
+        onClick={() =>
+          setItems((prev) => [
+            ...prev,
+            {
+              icon: faUser,
+              title: "User",
+            },
+          ])
+        }
+      >
+        Add item from nav
+      </button>
+      <button
+        onClick={() => setItems((prev) => prev.slice(0, prev.length - 1))}
+      >
+        Remove item from nav
+      </button>
       <Navbar label="Nav Menu">
-        <NavbarItem icon={faClock} title="Time" onSelect={handleItemClicked} />
-        <NavbarItem
-          icon={faFutbol}
-          title="Comments"
-          onSelect={handleItemClicked}
-        />
-        <NavbarItem
-          icon={faCompass}
-          title="Compass"
-          onSelect={handleItemClicked}
-        />
-        <NavbarItem icon={faUser} title="User" onSelect={handleItemClicked} />
+        {items.map((item, i) => (
+          <NavbarItem
+            key={`${item.title}-${i}`}
+            icon={item.icon}
+            title={item.title}
+            onSelect={handleItemClicked}
+          />
+        ))}
       </Navbar>
     </>
   );
